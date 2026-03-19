bd996144554fc71cab525292a8277bc506c27ae5
deepfriedslipper <20amelinesr@roundwoodpark.co.uk>
2026-03-19 12:31:05 +0000

    added zoom and move and cleaned up code

--- randomWalk/src/main.cpp
+++ randomWalk/src/main.cpp
@@ -1,68 +1,111 @@
 #include <SFML/Graphics.hpp>
-#include <SFML/System/Vector2.hpp>
+#include <SFML/System.hpp>
 
-//#include <math>
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <vector>
 
-#include "util/logger.hpp"
-#include "util/random.hpp"
+// PARAMETERS
 
+const int n = 10000;
+const int step = 20;
 
+std::mt19937 rng{ static_cast<std::mt19937::result_type>(std::chrono::steady_clock::now().time_since_epoch().count()) };
 
-struct pathVisualiser : sf::Drawable, sf::Transformable {
-	//pathvisualiser
-	
+class pathVisualiser : public sf::Drawable, public sf::Transformable {
+public:
 	pathVisualiser() {
 		m_vertices.setPrimitiveType(sf::LinesStrip);
+		//m_vertices.resize(n);
 	}
-	void add_vertex(sf::Vector2f pos,sf::Color colour) {
-		
-		m_vertices.append({ {pos.x,pos.y},colour });
+
+	void add_vertex(int i, sf::Vector2f pos,sf::Color colour) {
+		m_vertices.append({ pos, colour });
 	}
-	
+
 	sf::Vector2f lastVertex() {
 		return m_vertices[m_vertices.getVertexCount() - 1].position;
 	}
+
+	inline const sf::VertexArray& getVertices() {
+		return m_vertices;
+	}
+
 private:
-	sf::VertexArray m_vertices;
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
 		states.transform *= getTransform();
 		target.draw(m_vertices, states);
 	}
+
+private:
+	sf::VertexArray m_vertices;
 };
 
-//do we need more than 1 file for this one??
+float randomFloat() {
+	static constexpr const float X = rng.max();
+	return rng() / X;
+}
+
 int main() {
-	
-	//declare SFML required declarations
-	sf::RenderWindow window{ sf::VideoMode::getDesktopMode(), "line game.", sf::Style::Fullscreen };
-	int SCREEN_WIDTH = window.getSize().x;
-	int SCREEN_HEIGHT = window.getSize().y;
-	window.setTitle("random wlak");
+	sf::RenderWindow window{ { 1280, 720 }, "omg that's so rnadmo wlka", sf::Style::Default };
+
+	const int SCREEN_WIDTH = window.getSize().x;
+	const int SCREEN_HEIGHT = window.getSize().y;
+
 	pathVisualiser path;
-	int n=10000;
-	int step=20;
-	path.add_vertex({ float(SCREEN_WIDTH/2),float(SCREEN_HEIGHT/2) }, sf::Color::White);
-	for (int i = 0; i < n; i++) {
-		sf::Color colour(255 * random::randomFloat(), 255 * random::randomFloat(), 255 * random::randomFloat());
-		float angle = random::randomFloat() * 2 * 3.141592;
-		//placeholder for pi will fix later
-		float x = SCREEN_WIDTH;
-		float y = SCREEN_HEIGHT;
-		while (x >= SCREEN_WIDTH && y >= SCREEN_HEIGHT) {//check if escaped screen
-			x = path.lastVertex().x + step * sin(angle);
-			y = path.lastVertex().y + step * cos(angle);
-		}
-		
-		path.add_vertex({x,y}, colour);
+
+	path.add_vertex(0, { float(SCREEN_WIDTH / 2), float(SCREEN_HEIGHT / 2) }, sf::Color::White);
+
+	for (int i = 1; i < n; i++) {
+		sf::Color colour(255 * randomFloat(), 255 * randomFloat(), 255 * randomFloat());
+
+		float angle = randomFloat() * 2 * M_PI;
+
+		float x = path.lastVertex().x + step * sin(angle);
+		float y = path.lastVertex().y + step * cos(angle);
+
+		path.add_vertex(i, {x,y}, colour);
 	}
-	
+
+	sf::Vector2i previousMousePos;
+	bool dragging = false;
+
 	//the drawing only needs to happen once.
-	window.draw(path);
-	window.display();
-	bool running = 1;
-	while (running) {
-		
-		//not doing anything, just holding  the tab
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			switch (event.type) {
+				case sf::Event::Closed:
+					std::cout << "Window closed" << "\n";
+					window.close();
+					break;
+				case sf::Event::MouseButtonPressed:
+					dragging = true;
+					previousMousePos = sf::Mouse::getPosition(window);
+					break;
+				case sf::Event::MouseMoved:
+					if (dragging) {
+						sf::Vector2i mousepos = sf::Mouse::getPosition(window);
+						path.move(sf::Vector2f(mousepos - previousMousePos));
+						previousMousePos = mousepos;
+					}
+					break;
+				case sf::Event::MouseButtonReleased:
+					dragging = false;
+					break;
+				case sf::Event::MouseWheelScrolled:
+					sf::Vector2i mousepos = sf::Mouse::getPosition(window);
+					path.setOrigin(sf::Vector2f(mousepos) - path.getVertices().getBounds().getPosition());
+					path.scale(1.0f + event.mouseWheelScroll.delta, 1.0f + event.mouseWheelScroll.delta);
+					break;
+			}
+		}
+
+		window.clear();
+		window.draw(path);
+		window.display();
 	}
-	window.close();
 }
