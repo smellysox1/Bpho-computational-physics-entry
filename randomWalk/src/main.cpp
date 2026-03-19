8f33663a1a8a72ca51e19149b270139eaf0cdf57
Karl-MrX <20StocksJ@roundwoodpark.co.uk>
2026-03-19 09:48:27 +0000

    prevented escaping screen

--- randomWalk/src/main.cpp
+++ randomWalk/src/main.cpp
@@ -18,9 +18,7 @@ struct pathVisualiser : sf::Drawable, sf::Transformable {
 		
 		m_vertices.append({ {pos.x,pos.y},colour });
 	}
-	void test_n() {
-		WC2_LOG(m_vertices.getVertexCount());
-	}
+	
 	sf::Vector2f lastVertex() {
 		return m_vertices[m_vertices.getVertexCount() - 1].position;
 	}
@@ -41,19 +39,23 @@ int main() {
 	int SCREEN_HEIGHT = window.getSize().y;
 	window.setTitle("random wlak");
 	pathVisualiser path;
-	int n=1000;
-	int step=5;
+	int n=10000;
+	int step=20;
 	path.add_vertex({ float(SCREEN_WIDTH/2),float(SCREEN_HEIGHT/2) }, sf::Color::White);
 	for (int i = 0; i < n; i++) {
 		sf::Color colour(255 * random::randomFloat(), 255 * random::randomFloat(), 255 * random::randomFloat());
 		float angle = random::randomFloat() * 2 * 3.141592;
 		//placeholder for pi will fix later
-		float x = path.lastVertex().x+ step* sin(angle);
-		float y = path.lastVertex().y + step*cos(angle);
-		WC2_LOG(x," ", y);
+		float x = SCREEN_WIDTH;
+		float y = SCREEN_HEIGHT;
+		while (x >= SCREEN_WIDTH && y >= SCREEN_HEIGHT) {//check if escaped screen
+			x = path.lastVertex().x + step * sin(angle);
+			y = path.lastVertex().y + step * cos(angle);
+		}
+		
 		path.add_vertex({x,y}, colour);
 	}
-	path.test_n();
+	
 	//the drawing only needs to happen once.
 	window.draw(path);
 	window.display();
