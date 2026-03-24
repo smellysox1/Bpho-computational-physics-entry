fe38dffba869048258ba12caca6f69ddc078b459
deepfriedslipper <20amelinesr@roundwoodpark.co.uk>
2026-03-24 10:29:29 +0000

    moved dependencies to root and added gui for brownian motion

---
+++ brown-ian-motion/src/constants.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+unsigned int M_MIN = 1;
+unsigned int M_MAX = 100;
+
+unsigned int R_MIN = 1;
+unsigned int R_MAX = 100;
+
+unsigned int m_MIN = 1;
+unsigned int m_MAX = 100;
+
+unsigned int r_MIN = 1;
+unsigned int r_MAX = 100;
+
+float C_MIN = 0.0f;
+float C_MAX = 1.0f;
--- brown-ian-motion/src/main.cpp
+++ brown-ian-motion/src/main.cpp
@@ -10,80 +10,28 @@
 #include <vector>
 #include <filesystem>
 
-#define TAU 6.283185307179586
+#include "constants.hpp"
 
 // PARAMETERS
 
-unsigned int n = 11;
-unsigned int step = 20;
+unsigned int M = 5, m = 5, R = 5, r = 5;
+float C = 1;
 unsigned int seed = std::chrono::steady_clock::now().time_since_epoch().count();
 
 unsigned int screenWidth = 1280;
 unsigned int screenHeight = 720;
 
-class PathVisualiser : public sf::Drawable, public sf::Transformable {
-public:
-	void newPath() {
-		m_vertices.clear();
-
-		m_vertices.resize(n);
-		m_vertices.setPrimitiveType(sf::PrimitiveType::LineStrip);
-
-		m_vertices[0] = {{(float)(screenWidth / 2), (float)(screenHeight / 2)}, sf::Color::White};
-
-		for (int i = 1; i < n; i++) {
-			sf::Color colour(255 * randomFloat(colourRng), 255 * randomFloat(colourRng), 255 * randomFloat(colourRng));
-
-			float angle = randomFloat(angleRng) * TAU;
-
-			const sf::Vertex& previous = m_vertices[i - 1];
-
-			float x = previous.position.x + step * std::sinf(angle);
-			float y = previous.position.y + step * std::cosf(angle);
-
-			m_vertices[i] = {{x, y}, colour};
-		}
-	}
-
-	void resetRng(unsigned int s) {
-		angleRng = std::mt19937(s);
-	}
-
-private:
-	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
-		states.transform *= getTransform();
-		target.draw(m_vertices, states);
-	}
-
-	static float randomFloat(std::mt19937& rng) {
-		static constexpr const float max = rng.max();
-		return rng() / max;
-	}
-
-private:
-	sf::VertexArray m_vertices;
-
-	std::mt19937 angleRng {seed};
-	std::mt19937 colourRng {seed};
-};
+bool running = false;
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode({screenWidth, screenHeight}), "omg that's so rnadmo wlka", sf::Style::Default);
+	sf::RenderWindow window(sf::VideoMode({screenWidth, screenHeight}), "Brownian motion", sf::Style::Default);
 
 	if (!ImGui::SFML::Init(window)) {
 		std::cerr << "Could not initialise ImGui for SFML" << "\n";
 		return 1;
 	}
 
-	PathVisualiser path;
-	path.newPath();
-
 	sf::View camera(sf::FloatRect({0, 0}, {(float)screenWidth, (float)screenHeight}));
-
-	sf::Font font;
-	if (!font.openFromFile("arial.ttf")) {
-		std::cerr << "Could not load font." << "\n";
-	}
 
 	sf::Clock clock;
 
@@ -141,31 +89,42 @@ int main() {
 
 		window.clear();
 
-		ImGui::SetNextWindowSize({300.0f, 200.0f});
-		ImGui::Begin("Radnom walk", nullptr, ImGuiWindowFlags_NoResize);
+		ImGui::SetNextWindowSize({0.0f, 0.0f});
+		ImGui::Begin("Brownian Motion", nullptr, ImGuiWindowFlags_NoResize);
 
-		ImGui::Text("N:");
+		ImGui::Text("Mass of large particle:");
 		ImGui::SameLine();
-		ImGui::InputScalar("##n", ImGuiDataType_U32, &n);
+		ImGui::SliderScalar("##M", ImGuiDataType_U32, &M, &M_MIN, &M_MAX);
 
-		ImGui::Text("Step:");
+		ImGui::Text("Radius of large particle:");
 		ImGui::SameLine();
-		ImGui::InputScalar("##step", ImGuiDataType_U32, &step);
+		ImGui::SliderScalar("##R", ImGuiDataType_U32, &R, &R_MIN, &R_MAX);
 
-		ImGui::Text("Seed:");
+		ImGui::Text("Mass of small particles:");
 		ImGui::SameLine();
-		ImGui::InputScalar("##seed", ImGuiDataType_U32, &seed);
+		ImGui::SliderScalar("##m", ImGuiDataType_U32, &m, &m_MIN, &m_MAX);
 
-		if (ImGui::Button("Redraw")) {
-			path.resetRng(seed);
-			path.newPath();
+		ImGui::Text("Radius of small particles:");
+		ImGui::SameLine();
+		ImGui::SliderScalar("##r", ImGuiDataType_U32, &r, &r_MIN, &r_MAX);
+
+		ImGui::Text("Coefficient of Restitution:");
+		ImGui::SameLine();
+		ImGui::SliderFloat("##C", &C, C_MIN, C_MAX);
+
+		if (running || ImGui::Button("Step")) {
+			// update particles
+		}
+
+		if (ImGui::Button(running ? "Pause" : "Play")) {
+			running = !running;
 		}
 
 		ImGui::End();
 
 		window.setView(camera);
 
-		window.draw(path);
+		// draw
 
 		ImGui::SFML::Render(window);
 
